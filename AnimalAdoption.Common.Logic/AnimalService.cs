8042b46aabf8878bc3c912dd3a2b27c7f8cf1ed9
Braydon Zhao <braydon.zhao@thewarehouse.co.nz>
2021-03-18 12:01:24 +1300

    486

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,9 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Panda", Age = 5, Description = "Panda" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Panda A", Age = 5, Description = "Panda A" },
+            new Animal { Id = 2, Name = "Panda B", Age = 6, Description = "Panda B" },
+            new Animal { Id = 3, Name = "Panda C", Age = 7, Description = "Panda C" },
         };
     }
 }
